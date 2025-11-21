8a9f6235c2a1b8416451607070f1216028f3db10
Denis <denisgaldin@mail.ru>
2025-11-21 12:13:13 +0300

    Добавил тест 'Попытка обновить несуществующего питомца' и 'Попытка получить информацию о несуществующем питомце'

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -16,3 +16,32 @@ class TestPet:
 
         with allure.step('Проверка текстового содрежания ответа'):
             assert responce.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
+
+
+@allure.title('Попытка обновить несуществующего питомца')
+def test_update_nonexistent_pet(self):
+    with allure.step('Отправка запроса на обновление несуществующего питомца'):
+        payload = {
+            "id": 9999,
+            "name": "Non-existent Pet",
+            "status": "available"
+        }
+        responce = requests.put(url=f"{BASE_URL}/pet", json=payload)
+
+    with allure.step('Проверка статус кода'):
+        assert responce.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+    with allure.step('Проверка текстового содрежания ответа'):
+        assert responce.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
+
+
+@allure.title('Попытка получить информацию о несуществующем питомце')
+def test_get_nonexistent_pet(self):
+    with allure.step('Отправка запроса на получении информации о несущестующем питомце'):
+        responce = requests.get(url=f"{BASE_URL}/pet/9999")
+
+    with allure.step('Проверка статус кода'):
+        assert responce.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+    with allure.step('Проверка текстового содрежания ответа'):
+        assert responce.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
