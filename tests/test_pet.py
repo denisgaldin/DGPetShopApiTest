2967efb831ddb639853413767e84cf0fd354ec15
Denis <denisgaldin@mail.ru>
2025-11-21 13:34:51 +0300

    Добавил тест 'Попытка обновить несуществующего питомца' и 'Попытка получить информацию о несуществующем питомце'

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -6,7 +6,6 @@ BASE_URL = "http://5.181.109.28:9090/api/v3"
 
 @allure.feature("Pet")
 class TestPet:
-
     @allure.title("Попытка удалить несуществующего питомца")
     def test_delete_nonexistent_pet(self):
         with allure.step("Отправка запроса на удаление несуществующего питомца"):
