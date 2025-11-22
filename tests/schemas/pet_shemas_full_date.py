f951c196bb20e6bb0a3447e90a6a3fa4e174e30a
Denis <denisgaldin@mail.ru>
2025-11-22 17:24:55 +0300

    Добавил папку с json.schemas, добавил тесты: Добавление нового питомца, Добавление нового питомца c полными данными

---
+++ tests/schemas/pet_shemas_full_date.py
@@ -0,0 +1,52 @@
+PET_SCHEMA_FULL_DATE = {
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer"
+        },
+        "name": {
+            "type": "string"
+        },
+        "category": {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "integer"
+                },
+                "name": {
+                    "type": "string"
+                }
+            },
+            "required": ["id", "name"],
+            "additionalProperties": False
+        },
+        "photoUrls": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "tags": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "property": {
+                    "id": {
+                        "type": "string"}
+                },
+                "name": {
+                    "type": "string"
+                }
+            },
+            "required": ["id", "name"],
+            "additionalProperties": False
+        },
+        "status": {
+            "type": "string",
+            "enum": ["available", "pending", "sold"]
+            }
+    },
+    "required": ["id", "name", "photoUrls", "status"],
+    "additionalProperties": False
+    }
+
